da913db5418bc357191b13ce375c60efb97e43f3
ribbery009 <dudas.peter.ovb@gmail.com>
2020-10-28 13:32:16 +0100

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import axios from 'axios';
+import post from '../../components/Post/Post';
+
+class Blog extends Component {
+    state={
+        posts: []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then
+        (response =>
+        {
+            const posts=response.data.slice(0, 4);
+            const updatedPosts= posts.map(post => {
+            
+            return {
+                ...post, author: 'Peter'
+            }
+            })
+            this.setState({posts: updatedPosts});
+        }
+        );
+        
+    }
+    
+    render () {
+const posts= this.state.posts.map(post =>
+    {
+        return <Post key={post.id} title={post.title} author={post.author}/>;
+    })
+        return (
+            <div>
+                <section className="Posts">
+                   {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
